77bcaff3eda3a87e9e41dd90e44a255130440f36
bastosanaa <bastosanaa@gmail.com>
2025-03-25 15:58:59 -0300

    Feat: create signup page

--- login-page/src/app/services/login.service.ts
+++ login-page/src/app/services/login.service.ts
@@ -2,16 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from '../types/login-response.type';
 import { tap } from 'rxjs';
+import { apiUrl } from './apiUrl';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
+  
   constructor(private httpClient: HttpClient) { }
+  
+  login(email:string, password: string) {
+    const url = apiUrl + "/login"
+    return this.httpClient.post<LoginResponse>(url, {email, password}).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
 
-  login(name:string, password: string) {
-    return this.httpClient.post<LoginResponse>("/login", {name, password}).pipe(
+      })
+    )
+  }
+
+  signup(name:string, email:string, password: string) {
+    const url = apiUrl + "/register"
+    return this.httpClient.post<LoginResponse>(url, {name, email, password}).pipe(
       tap((value) => {
         sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
